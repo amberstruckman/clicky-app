e0c7c52039c3ef7783e6a20adacd341dbe6155cc
Amber <amber.r.struckman@gmail.com>
2019-01-10 18:41:17 -0800

    deployment fixed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React, { Component } from 'react';
-import Table from './Table';
-import Form from './Form';
+// import Table from './Table';
+// import Form from './Form';
 import Nav from './Nav'
 import Footer from './Footer'
 import PhotoGrid from './PhotoGrid'
 
 class App extends Component {
-    state = {
-        characters: []
-    };
+    // state = {
+    //     characters: []
+    // };
 
-    removeCharacter = index => {
-        const { characters } = this.state;
+    // removeCharacter = index => {
+    //     const { characters } = this.state;
     
-        this.setState({
-            characters: characters.filter((character, i) => { 
-                return i !== index;
-            })
-        });
-    }
+    //     this.setState({
+    //         characters: characters.filter((character, i) => { 
+    //             return i !== index;
+    //         })
+    //     });
+    // }
 
-    handleSubmit = character => {
-        this.setState({characters: [...this.state.characters, character]});
-    }
+    // handleSubmit = character => {
+    //     this.setState({characters: [...this.state.characters, character]});
+    // }
 
     render() {
-        const { characters } = this.state;
+        // const { characters } = this.state;
         
         return (
             <div className="container">
