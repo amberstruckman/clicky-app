433defb34cd90ec7df97714e7c63bbfd7d88b000
Amber <amber.r.struckman@gmail.com>
2019-01-28 23:19:47 -0800

    Updates

---
+++ precache-manifest.bfb20ea8d4d6629fba065a5a745930d2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "651d25da8edbb973958703469fb8be4b",
+    "url": "/clicky-app/static/media/Jigglypuff_Pokemon.651d25da.png"
+  },
+  {
+    "revision": "53b702e9c69186e8173d",
+    "url": "/clicky-app/static/css/main.9c98659d.chunk.css"
+  },
+  {
+    "revision": "fd4e80d74e930666c782",
+    "url": "/clicky-app/static/js/1.fd4e80d7.chunk.js"
+  },
+  {
+    "revision": "f99a7f748a54c0d52a3c",
+    "url": "/clicky-app/static/js/runtime~main.f99a7f74.js"
+  },
+  {
+    "revision": "4e762991064810956006546a8a81b776",
+    "url": "/clicky-app/static/media/Bulbasaur_Pokemon.4e762991.png"
+  },
+  {
+    "revision": "078eeb3a8aa5cb69552e858f2ec1ad1b",
+    "url": "/clicky-app/static/media/Charmander_Pokemon.078eeb3a.png"
+  },
+  {
+    "revision": "7a492aaf574aab0f7def37ac0887a535",
+    "url": "/clicky-app/static/media/Eevee_Pokemon.7a492aaf.png"
+  },
+  {
+    "revision": "ee84a2cfab2986ab38e938d3106af5ff",
+    "url": "/clicky-app/static/media/Fins_Pokemon.ee84a2cf.png"
+  },
+  {
+    "revision": "53b702e9c69186e8173d",
+    "url": "/clicky-app/static/js/main.53b702e9.chunk.js"
+  },
+  {
+    "revision": "bce1cb861252fd19ae4c13ee56e75fdd",
+    "url": "/clicky-app/static/media/Kirlia_Pokemon.bce1cb86.png"
+  },
+  {
+    "revision": "a6456fc5497021d2519e66875132993b",
+    "url": "/clicky-app/static/media/Marill_Pokemon.a6456fc5.png"
+  },
+  {
+    "revision": "27867d7d004f55c7965b2388e4260aff",
+    "url": "/clicky-app/static/media/Pachirisu_Pokemon.27867d7d.png"
+  },
+  {
+    "revision": "f027ab7bf8175043657dc2d1ed4b8144",
+    "url": "/clicky-app/static/media/Pidgey_Pokemon.f027ab7b.png"
+  },
+  {
+    "revision": "f12b69cfab4acf28a3b90ad2167b403c",
+    "url": "/clicky-app/static/media/Pika_Pokemon.f12b69cf.png"
+  },
+  {
+    "revision": "76415f228943787b7ae6c93e0936623a",
+    "url": "/clicky-app/static/media/Piplup_Pokemon.76415f22.png"
+  },
+  {
+    "revision": "74123a5f8c1189eb610c00b3f64a9b58",
+    "url": "/clicky-app/static/media/Spike_Pokemon.74123a5f.png"
+  },
+  {
+    "revision": "98b16e5bc63b274f053ff97cf064becc",
+    "url": "/clicky-app/index.html"
+  }
+];
